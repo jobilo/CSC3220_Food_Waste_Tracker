99326bea47e1d299a6d213f8294046a3c61fd505
Jobi Lo <102559432+jobilo@users.noreply.github.com>
2023-06-06 10:55:58 -0700

    SQLite Database

---
+++ FoodTrackerApp/components/sqlDatabase.js
@@ -0,0 +1,120 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import * as SQLite from 'expo-sqlite'
+import { useState, useEffect } from "react";
+
+export default function App() {
+    const [db, setDb] = useState(SQLite.openDatabase('example.db'));
+    const [isLoading, setIsLoading] = useState(true);
+    const [names, setNames] = useState([]);
+    const [currentName, setCurrentName] = useState(undefined);
+
+    useEffect(() => {
+        db.transaction(tx => {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+        });
+
+
+        db.transaction(tx => {
+            tx.executeSql('SELECT * FROM names', null,
+                (txObj, resultSet) => setNames(resultSet.rows._array),
+                (txObj, error) => console.log(error)
+            );
+        });
+
+        setIsLoading(false);
+    }, [db]);
+
+    if (isLoading) {
+        return (
+            <View style={styles.container}>
+                <Text>Loading names...</Text>
+            </View>
+        );
+    }
+
+    const addName = () => {
+        db.transaction(tx => {
+            tx.executeSql('INSERT INTO names (name) values (?)', [currentName],
+                (txObj, resultSet) => {
+                    let existingNames = [...names];
+                    existingNames.push({ id: resultSet.insertId, name: currentName });
+                    setNames(existingNames);
+                    setCurrentName(undefined);
+                },
+                (txObj, error) => console.log(error)
+            );
+        });
+    }
+
+    const deleteName = (id) => {
+        db.transaction(tx => {
+            tx.executeSql('DELETE FROM names WHERE id = ?', [id],
+                (txObj, resultSet) => {
+                    if (resultSet.rowsAffected > 0) {
+                        let existingNames = [...names].filter(name => name.id !== id);
+                        setNames(existingNames);
+                    }
+                },
+                (txObj, error) => console.log(error)
+            );
+        });
+    };
+
+    const updateName = (id) => {
+        db.transaction(tx => {
+            tx.executeSql('UPDATE names SET name = ? WHERE id = ?', [currentName, id],
+                (txObj, resultSet) => {
+                    if (resultSet.rowsAffected > 0) {
+                        let existingNames = [...names];
+                        const indexToUpdate = existingNames.findIndex(name => name.id === id);
+                        existingNames[indexToUpdate].name = currentName;
+                        setNames(existingNames);
+                        setCurrentName(undefined);
+                    }
+                },
+                (txObj, error) => console.log(error)
+            );
+        });
+    };
+
+    const showNames = () => {
+        return names.map((name, index) => {
+            return (
+                <View key={index} style={styles.row}>
+                    <Text>{name.name}</Text>
+                    <Button title='Delete' onPress={() => deleteName(name.id)} />
+                    <Button title='Update' onPress={() => updateName(name.id)} />
+                </View>
+            );
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                value={currentName}
+                placeholder='Enter name'
+                onChangeText={setCurrentName} />
+            <Button title="Add Name" onPress={addName}></Button>
+            {showNames()}
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        justifyContent: 'space-between',
+        margin: 8
+    }
+});
